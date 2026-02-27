34b3d0e2222b8cbd1e42784122531246246a44ff
Fatima20027 <fatima.hailou@icloud.com>
2026-02-27 00:31:54 +0000

    fixed contact button

--- 3-landing-page/src/Componenets/Navbar.jsx
+++ 3-landing-page/src/Componenets/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           ))}
           
         </ul>
-        <button className="flex px-6 py-2  text-white bg-slate-900 font-bold rounded-full    shadow-sm  transition-colors cursor-pointer border border-slate-900 hover:text-slate-900 hover:bg-transparent duration-300   ">
+        <button className="flex px-6 py-2  text-white bg-slate-900 font-bold rounded-full    shadow-sm  transition-colors cursor-pointer border border-slate-900 hover:brightness-130 duration-300   ">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="md:size-4 size-5 relative right-1.5 md:top-1 bottom-0.5 font-bold">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
             </svg>
@@ -64,7 +64,7 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <button className=" flex justify-center mt-5 w-full py-3 text-white border bg-slate-900 focus:border-slate-900 focus:text-slate-900 focus:bg-transparent font-bold rounded-full shadow-sm active:scale-95 transition-transform duration-300">
+        <button className=" flex justify-center mt-5 w-full py-3 text-white  bg-slate-900 focus:brightness-130   font-bold rounded-full shadow-sm active:scale-95 transition-transform duration-300">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="md:size-4 size-5 relative right-1.5 md:top-0 ">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
           </svg>
